f73003c82ac6813f778314e2c9d12a0627d27a55
suresh <suresh.gadde@iiitb.org>
2020-05-29 08:35:44 +0530

    modified spec

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -37,31 +37,31 @@ describe("Server", () => {
     // });
 
 
-    // // describe("SWIGGY", () => {
-    // //    var data, data1;
-    // //    var keys, keys1;
-    // //    var urls;
-    // //     beforeAll(async (done) => {
-    // //            await swiggy.initialize("banglore");
-    // //            urls = await swiggy.getUrls();
-    // //            data = await swiggy.retrieve("https://www.swiggy.com/restaurants/rolls-on-wheels-woods-street-central-bangalore-bangalore-9103");
-    // //             keys = Object.keys(data);
-    // //             data1 = await swiggy.bestseller();
-    // //             keys1 = Object.keys(data1[0]);
+    describe("SWIGGY", () => {
+       var data, data1;
+       var keys, keys1;
+       var urls;
+        beforeAll(async (done) => {
+               await swiggy.initialize("banglore");
+               urls = await swiggy.getUrls();
+               data = await swiggy.retrieve("https://www.swiggy.com/restaurants/rolls-on-wheels-woods-street-central-bangalore-bangalore-9103");
+                keys = Object.keys(data);
+                data1 = await swiggy.bestseller();
+                keys1 = Object.keys(data1[0]);
                 
-    // //             done();
+                done();
                 
-    // //     }, 500000);
-    // //     it("description of single item of a restaurant", () => {
-    // //         expect(keys).toEqual(["restaurantName","tag", "address", "rating", "peopleRated", "weblink", "items"]);
-    // //     });
-    // //     it("best seller restaurants in the town", () => {
-    // //         expect(keys1).toEqual(["restaurantName","tag", "rating", "sgylink"]);
-    // //     });
-    // //     it("restaurant urls", () => {
-    // //         expect(typeof(urls)).toBe("object");
-    // //     });
-    // // });
+        }, 500000);
+        it("description of single item of a restaurant", () => {
+            expect(keys).toEqual(["restaurantName","tag", "address", "rating", "peopleRated", "weblink", "items"]);
+        });
+        it("best seller restaurants in the town", () => {
+            expect(keys1).toEqual(["restaurantName","tag", "rating", "sgylink"]);
+        });
+        it("restaurant urls", () => {
+            expect(typeof(urls)).toBe("object");
+        });
+    });
    
     // // // describe("GET /test", () => {
     //     var data = {};
